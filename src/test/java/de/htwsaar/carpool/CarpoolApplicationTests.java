be0c55d870d3219ba0df00f0ba14a1b02568cc80
Ernesto Miranda <55798237+ErnoMitrovic@users.noreply.github.com>
2025-02-16 10:21:40 +0100

    Usng docker internal

--- src/test/java/de/htwsaar/carpool/CarpoolApplicationTests.java
+++ src/test/java/de/htwsaar/carpool/CarpoolApplicationTests.java
@@ -1,6 +1,7 @@
 package de.htwsaar.carpool;
 
 import com.redis.testcontainers.RedisContainer;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,32 +12,39 @@ import org.testcontainers.containers.GenericContainer;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
 class CarpoolApplicationTests {
 
-	static final GenericContainer<?> redis = new RedisContainer("redis:6.2.6")
-			.withExposedPorts(6379)
-			.withReuse(true);
+    static final GenericContainer<?> redis = new RedisContainer("redis:6.2.6")
+            .withExposedPorts(6379)
+            .withReuse(true);
 
-	static {
-		redis.start();
-	}
+    static {
+        redis.start();
+    }
 
-	@DynamicPropertySource
-	static void configureRedis(DynamicPropertyRegistry registry) {
-		registry.add("spring.data.redis.host", () -> "localhost");
-		registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
-	}
+    @DynamicPropertySource
+    static void configureRedis(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", () -> {
+            log.info("Redis host: {}", redis.getHost());
+            if (redis.getHost().startsWith("[tcp://")) {
+                return "host.docker.internal";
+            }
+            return redis.getHost();
+        });
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+    }
 
-	@AfterAll
-	static void stopRedis(){
-		redis.stop();
-	}
+    @AfterAll
+    static void stopRedis() {
+        redis.stop();
+    }
 
-	@Test
-	void containerIsRunning() {
-		assertThat(redis.isRunning()).isTrue();
-	}
+    @Test
+    void containerIsRunning() {
+        assertThat(redis.isRunning()).isTrue();
+    }
 
 }
